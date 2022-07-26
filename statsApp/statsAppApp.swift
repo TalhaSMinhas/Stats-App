c844dd5f60720db54e2d3d9885c43034953112c1
TalhaSMinhas <talha.salman.minhas@icloud.com>
2022-07-26 11:32:46 +0200

    Initial Commit

---
+++ statsApp/statsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  statsAppApp.swift
+//  statsApp
+//
+//  Created by Talha Minhas on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct statsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
